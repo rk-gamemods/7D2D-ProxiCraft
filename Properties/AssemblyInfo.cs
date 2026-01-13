85ca8eb20545b9f1c0e9c438d15c81f343644355
Robert Knighton <robert.knighton@gmail.com>
2026-01-12 22:46:42 -0500

    Update AssemblyInfo to 1.2.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
 
 [assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyFileVersion("1.2.7.0")]
-[assembly: AssemblyInformationalVersion("1.2.7")]
-[assembly: AssemblyVersion("1.2.7.0")]
+[assembly: AssemblyFileVersion("1.2.8.0")]
+[assembly: AssemblyInformationalVersion("1.2.8")]
+[assembly: AssemblyVersion("1.2.8.0")]
